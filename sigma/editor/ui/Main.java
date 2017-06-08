6730572dc4067470570d75868437b7f91cdc1224
BrandonQuinn <brandonquinnn@gmail.com>
2017-06-08 11:10:25 +1000

    Init

---
+++ sigma/editor/ui/Main.java
@@ -0,0 +1,16 @@
+package sigma.editor.ui;
+
+/**
+ * A level editor for the Sigma game.
+ * 
+ * @author Brandon Quinn
+ * @version 0.1
+ * @since 8 Jun 2017
+ */
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+	}
+}
