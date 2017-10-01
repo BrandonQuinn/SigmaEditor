5028de09d13b7db134720063daf691649779877e
BrandonQuinn <brandonquinnn@gmail.com>
2017-10-02 01:51:01 +1100

    Dependency Apache Commons removed

--- sigma/editor/config/loaders/GeneralConfLoader.java
+++ sigma/editor/config/loaders/GeneralConfLoader.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
